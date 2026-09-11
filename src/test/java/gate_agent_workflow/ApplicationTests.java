f6d2b12f0af785923c0d223f90d3005fcc202f48
Rodolfo Giacomodonatto <rodol.giacomodonatto@gmail.com>
2026-09-11 16:30:17 -0300

    feat(kit):  update logic for pull for structure proyect

---
+++ src/test/java/gate_agent_workflow/ApplicationTests.java
@@ -0,0 +1,13 @@
+package gate_agent_workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
